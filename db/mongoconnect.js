8c60bdfc53cf3022cfd9a41de780da3010a881a9
TamarShwartz <tamar3205471@gmail.com>
2023-12-03 12:27:18 +0200

    commit

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -5,9 +5,8 @@ const { config } = require("../config/secret")
 main().catch(err => console.log(err));
 
 async function main() {
-  mongoose.set('strictQuery' , false);
-
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.z2vr2va.mongodb.net/test`);
+ mongoose.set('strictQuery' , false);
+ await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.z2vr2va.mongodb.net/test`);
   console.log("mongo connect started");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
 }
